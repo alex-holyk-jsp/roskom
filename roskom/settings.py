6c49f84bbe85c84a5244ce3c62329912a45edc5c
Alex Holyk <oleksandr.holyk@jetsoftpro.com>
2020-09-03 16:27:33 +0300

    use env variable for django SECRET_KEY

--- roskom/settings.py
+++ roskom/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5rgkke+!*mes6rn3a%x#td!cc=e%k4l4e#41tbmpnzg@q28qb3'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
