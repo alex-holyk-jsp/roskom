5816d2c63fdb18907ae33442b10dd7fc3e81be19
Alex Holyk <oleksandr.holyk@jetsoftpro.com>
2020-09-04 11:53:11 +0300

    add owner_ip to Domains

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ class Domain(models.Model):
     created = models.DateTimeField(auto_now=True)
     domain = models.CharField(max_length=200, blank=True, default='')
     ip = models.CharField(max_length=15)
+    owner_ip = models.CharField(max_length=15)
 
     class Meta:
         ordering = ['created']
--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@ from .models import Domain
 
 
 class DomainSerializer(serializers.ModelSerializer):
+    owner_ip = serializers.ReadOnlyField()
+
     class Meta:
         model = Domain
-        fields = ['id', 'domain', 'ip']
+        fields = ['id', 'domain', 'ip', 'owner_ip']
