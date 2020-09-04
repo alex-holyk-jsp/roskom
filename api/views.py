7f3e660a42d1798f9ce973fa1c1ef91d4e256094
Alex Holyk <70570449+alex-holyk-jsp@users.noreply.github.com>
2020-09-04 16:17:41 +0300

    Feature/add admin (#6)
    * add sign in
    
    * add permissions to update Domains only for authorized user

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Domain
 from .serializers import DomainSerializer
@@ -12,3 +12,10 @@ class DomainViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         client_ip = get_client_ip(self.request)
         serializer.save(owner_ip=client_ip)
+
+    def get_permissions(self):
+        if self.action in ('update', 'partial_update', 'destroy'):
+            permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
