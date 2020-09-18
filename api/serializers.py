c7859de49669a04cf2cf57b5fecacb3a97358cc9
Alex Holyk <70570449+alex-holyk-jsp@users.noreply.github.com>
2020-09-18 12:06:20 +0300

    send email on status approve (#14)

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,14 @@ from .models import Domain
 
 class BaseDomainSerializer(serializers.ModelSerializer):
     owner_ip = serializers.ReadOnlyField()
+    owner_email = serializers.ReadOnlyField()
 
     class Meta:
         model = Domain
-        fields = ['id', 'domain', 'ip', 'owner_ip', 'status']
+        fields = ['id', 'domain', 'ip', 'owner_ip', 'status', 'owner_email']
 
 
 class AnonymousUserDomainSerializer(BaseDomainSerializer):
     status = serializers.ReadOnlyField()
+    owner_email = serializers.EmailField(max_length=200)
+
